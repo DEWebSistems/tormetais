77f214250b3099f483f7eb04ed079168ae4312a0
everaldoboccoli <everaldo.boccoli@gmail.com>
2013-11-02 15:50:25 -0200

    Outros arquivos

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+		<meta charset="utf-8">
+		<title>Food Shop</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="author" content="Everaldo" >
+		
+		<!-- Styles -->
+                <link rel="stylesheet" href="assets/css/layout.css" type="text/css"/>
+                <link rel="stylesheet" href="assets/bootstrap/3.0.0/css/bootstrap.css" type="text/css">
+		
+		<!-- Java Script -->
+		<script type="text/javascript" src="assets/jquery/jquery203/jquery-2.0.3.js"></script>
+		<script type="text/javascript" src="assets/bootstrap/3.0.0/js/bootstrap.js"></script>
+	</head>
+	
+	<body>
+	<div class="content">
+
+		<!-- <CABECALHO> -->
+		<div class="header">
+			<div class="container">
+				<div class="row header-content">
+					teste
+				</div>
+			</div>
+		</div>
+                <br/>
+
+		<!-- </CABECALHO> -->
+
+		<div class="menu">
+			menu
+		</div>
+
+		<div class="main">
+			
+			content
+		</div>
+
+
+		<!-- <RODAPÉ> -->
+
+
+		<div class="footer">
+			<div class="container">
+				<div class="row footer-content">
+					<div class="col-md-4">1</div>
+					<div class="col-md-4">2</div>
+					<div class="col-md-4">3</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	</body>
+</html>
